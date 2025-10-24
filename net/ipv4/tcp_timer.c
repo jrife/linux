5b2ce3fae29b6e432d508e818b6942bfa7f16f8c
Jordan Rife <jordan@jrife.io>
2025-10-24 11:23:07 -0700

    bpf: Support ETIMEDOUT reply from BPF_SOCK_OPS_RTO_CB
    Cilium supports a mode where it uses SOCK_ADDR hooks (cgroup/connect4,
    cgroup/sendmsg4, ...) to do socket-level load balancing, translating
    service VIPs to real backend IPs. This is more efficient than per-packet
    translation for TCP and connected UDP, but there's a drawback: if the
    selected backend goes away then socket keeps trying to send traffic to
    it. This is remedied today by iterating through sockets using BPF socket
    iterators whenever a backend is deleted and destroying any sockets
    connected to that backend with bpf_sock_destroy(). This works, but it
    doesn't scale well as the number of sockets, backend deletions, or
    network namespaces increases [1].
    
    As an alternative for TCP sockets, allow the BPF_SOCK_OPS_RTO_CB sockops
    callback to accelerate socket expiration by returning -ETIMEDOUT. This
    enables quick destruction of TCP sockets connected to a deleted backend
    without additional fast path overhead or explicit socket iteration.
    
    Example showing the intended use:
    
      SEC("sockops")
      int handler(struct bpf_sock_ops *skops)
      {
          struct bpf_sock *sk;
          __u32 *backend_id;
    
          sk = skops->sk;
          if (!sk)
              return 1;
    
          if (skops->op == BPF_SOCK_OPS_RTO_CB) {
              backend_id = bpf_sk_storage_get(&socket_backends, sk, NULL, 0);
              if (backend_id && !bpf_map_lookup_elem(&backends, backend_id)) {
                  /* Backend no longer exists, so give up. */
                  skops->reply = -ETIMEDOUT;
                  return 1;
              }
          }
    
          return 0;
      }
    
    [1]: https://lore.kernel.org/bpf/20250909170011.239356-1-jordan@jrife.io/
    
    Suggested-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: Jordan Rife <jordan@jrife.io>

--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -285,9 +285,10 @@ static int tcp_write_timeout(struct sock *sk)
 	mptcp_active_detect_blackhole(sk, expired);
 
 	if (BPF_SOCK_OPS_TEST_FLAG(tp, BPF_SOCK_OPS_RTO_CB_FLAG))
-		tcp_call_bpf_3arg(sk, BPF_SOCK_OPS_RTO_CB,
-				  icsk->icsk_retransmits,
-				  icsk->icsk_rto, (int)expired);
+		expired |= tcp_call_bpf_3arg(sk, BPF_SOCK_OPS_RTO_CB,
+					     icsk->icsk_retransmits,
+					     icsk->icsk_rto,
+					     (int)expired) == -ETIMEDOUT;
 
 	if (expired) {
 		/* Has it gone just too far? */
