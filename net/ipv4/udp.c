185b45a8330943230c7e0663a6037d6d364d9029
Jordan Rife <jordan@jrife.io>
2026-04-11 16:43:12 -0700

    try something else

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -368,7 +368,7 @@ int udp_v4_get_port(struct sock *sk, unsigned short snum)
 static int compute_score(struct sock *sk, const struct net *net,
 			 __be32 saddr, __be16 sport,
 			 __be32 daddr, unsigned short hnum,
-			 int dif, int sdif, int dscore)
+			 int dif, int sdif)
 {
 	int score;
 	struct inet_sock *inet;
@@ -388,13 +388,13 @@ static int compute_score(struct sock *sk, const struct net *net,
 	if (inet->inet_daddr) {
 		if (inet->inet_daddr != saddr)
 			return -1;
-		score += dscore;
+		score += 4;
 	}
 
 	if (inet->inet_dport) {
 		if (inet->inet_dport != sport)
 			return -1;
-		score += dscore;
+		score += 4;
 	}
 
 	dev_match = udp_sk_bound_dev_eq(net, sk->sk_bound_dev_if,
@@ -407,6 +407,19 @@ static int compute_score(struct sock *sk, const struct net *net,
 	if (READ_ONCE(sk->sk_incoming_cpu) == raw_smp_processor_id())
 		score++;
 	return score;
+}
+
+static inline bool score_needs_adjustment(struct sock *sk)
+{
+	return sk->sk_state == TCP_CLOSE && inet_sk(sk)->inet_daddr;
+}
+
+static inline int adjust_score(struct sock *sk, int score)
+{
+	if (inet_sk(sk)->inet_dport)
+		score -= 4;
+
+	return score - 4;
 }
 
 u32 udp_ehashfn(const struct net *net, const __be32 laddr, const __u16 lport,
@@ -452,8 +465,7 @@ static struct sock *udp4_lib_lookup1(const struct net *net,
 
 	sk_for_each_rcu(sk, &hslot->head) {
 		score = compute_score(sk, net,
-				      saddr, sport, daddr, hnum, dif, sdif,
-				      sk->sk_state == TCP_CLOSE ? 0 : 4);
+				      saddr, sport, daddr, hnum, dif, sdif);
 		if (score > badness) {
 			result = sk;
 			badness = score;
@@ -474,6 +486,7 @@ static struct sock *udp4_lib_lookup2(const struct net *net,
 	struct sock *sk, *result;
 	int score, badness;
 	bool need_rescore;
+	__u8 state;
 
 	result = NULL;
 	badness = 0;
@@ -481,15 +494,20 @@ static struct sock *udp4_lib_lookup2(const struct net *net,
 		need_rescore = false;
 rescore:
 		score = compute_score(need_rescore ? result : sk, net, saddr,
-				      sport, daddr, hnum, dif, sdif,
-				      sk->sk_state == TCP_CLOSE ? 0 : 4);
+				      sport, daddr, hnum, dif, sdif);
 		if (score > badness) {
+			state = sk->sk_state;
+			if (unlikely(state == TCP_CLOSE &&
+				     inet_sk(sk)->inet_daddr))
+				if (adjust_score(sk, score) <= badness)
+					continue;
+
 			badness = score;
 
 			if (need_rescore)
 				continue;
 
-			if (sk->sk_state == TCP_ESTABLISHED) {
+			if (state == TCP_ESTABLISHED) {
 				result = sk;
 				continue;
 			}
